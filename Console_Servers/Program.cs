fa05d35153e45658a9b4a6a66ef30c3be93680f1
natac <natac@EUGENE>
2024-05-13 18:00:29 +0700

    Добавьте файлы проекта.

---
+++ Console_Servers/Program.cs
@@ -0,0 +1,32 @@
+﻿using Servers_For_Server;
+
+namespace Console_Servers
+{
+    class Program
+    {
+        static void Main()
+        {
+            Servers servers = Servers.GetInstance();
+
+            Console.WriteLine("Добавление серверов:");
+            Console.WriteLine("Добавляем 'http://example.com': " + servers.AddServer("http://example.com"));
+            Console.WriteLine("Добавляем 'https://example.org': " + servers.AddServer("https://example.org"));
+            Console.WriteLine("Добавляем 'ftp://notallowed.com': " + servers.AddServer("ftp://notallowed.com")); // Не добавится из-за префикса
+            Console.WriteLine("Добавляем 'http://example.com': " + servers.AddServer("http://example.com")); // Дубликат - не добавится
+
+            Console.WriteLine("\nHTTP сервера:");
+            var httpServers = servers.GetHttpServers();
+            foreach (var server in httpServers)
+            {
+                Console.WriteLine(server);
+            }
+
+            Console.WriteLine("\nHTTPS сервера:");
+            var httpsServers = servers.GetHttpsServers();
+            foreach (var server in httpsServers)
+            {
+                Console.WriteLine(server);
+            }
+        }
+    }
+}
---
+++ Servers_For_Server/Servers.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Servers_For_Server
+{
+    public class Servers
+    {
+        private static readonly Servers instance = new Servers();
+        private List<string> serverList;
+        private static readonly object lockObject = new object();
+        private Servers()
+        {
+            serverList = new List<string>();
+        }
+        public static Servers GetInstance()
+        {
+            return instance;
+        }
+        public bool AddServer(string serverAddress)
+        {
+            lock (lockObject)
+            {
+                if (serverAddress.StartsWith("http://") || serverAddress.StartsWith("https://"))
+                {
+                    if (!serverList.Contains(serverAddress))
+                    {
+                        serverList.Add(serverAddress);
+                        return true;
+                    }
+                }
+                return false;
+            }
+        }
+        public List<string> GetHttpServers()
+        {
+            lock (lockObject)
+            {
+                return serverList.FindAll(server => server.StartsWith("http://"));
+            }
+        }
+        public List<string> GetHttpsServers()
+        {
+            lock (lockObject)
+            {
+                return serverList.FindAll(server => server.StartsWith("https://"));
+            }
+        }
+    }
+}
