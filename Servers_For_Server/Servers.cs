01eb5dc35cbb30e78d4de183ae9f7164011040be
NAumenko12 <143539153+NAumenko12@users.noreply.github.com>
2024-05-15 16:47:35 +0700

    Update Servers.cs

--- Servers_For_Server/Servers.cs
+++ Servers_For_Server/Servers.cs
@@ -7,46 +7,62 @@ using System.Threading.Tasks;
 namespace Servers_For_Server
 {
     public class Servers
-    {
-        private static readonly Servers instance = new Servers();
-        private List<string> serverList;
-        private static readonly object lockObject = new object();
-        private Servers()
-        {
-            serverList = new List<string>();
-        }
-        public static Servers GetInstance()
-        {
-            return instance;
-        }
-        public bool AddServer(string serverAddress)
-        {
-            lock (lockObject)
-            {
-                if (serverAddress.StartsWith("http://") || serverAddress.StartsWith("https://"))
-                {
-                    if (!serverList.Contains(serverAddress))
-                    {
-                        serverList.Add(serverAddress);
-                        return true;
-                    }
-                }
-                return false;
-            }
-        }
-        public List<string> GetHttpServers()
-        {
-            lock (lockObject)
-            {
-                return serverList.FindAll(server => server.StartsWith("http://"));
-            }
-        }
-        public List<string> GetHttpsServers()
-        {
-            lock (lockObject)
-            {
-                return serverList.FindAll(server => server.StartsWith("https://"));
-            }
-        }
-    }
+ {
+     // Жадная инициализация
+     private static readonly Servers eagerInstance = new Servers();
+     // Ленивая инициализация
+     private static Lazy<Servers> lazyInstance = new Lazy<Servers>(() => new Servers());
+
+     private List<string> serverList;
+     private static readonly object lockObject = new object();
+
+     private Servers()
+     {
+         serverList = new List<string>();
+     }
+
+     // Метод для получения жадной версии
+     public static Servers GetEagerInstance()
+     {
+         return eagerInstance;
+     }
+
+     // Метод для получения ленивой версии
+     public static Servers GetLazyInstance()
+     {
+         return lazyInstance.Value;
+     }
+
+     public bool AddServer(string serverAddress)
+     {
+         lock (lockObject)
+         {
+             if (serverAddress.StartsWith("http://") || serverAddress.StartsWith("https://"))
+             {
+                 if (!serverList.Contains(serverAddress))
+                 {
+                     serverList.Add(serverAddress);
+                     return true;
+                 }
+             }
+             return false;
+         }
+     }
+
+     public List<string> GetHttpServers()
+     {
+         lock (lockObject)
+         {
+             return serverList.FindAll(server => server.StartsWith("http://"));
+         }
+     }
+
+     public List<string> GetHttpsServers()
+     {
+         lock (lockObject)
+         {
+             return serverList.FindAll(server => server.StartsWith("https://"));
+         }
+     }
+ }
 }
