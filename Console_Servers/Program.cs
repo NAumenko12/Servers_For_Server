26596f474bf87bc5b6bfca6534bc4c99b6807db6
NAumenko12 <143539153+NAumenko12@users.noreply.github.com>
2024-05-15 16:48:45 +0700

    Update Program.cs

--- Console_Servers/Program.cs
+++ Console_Servers/Program.cs
@@ -4,25 +4,28 @@ namespace Console_Servers
 {
     class Program
     {
-        static void Main()
+            static void Main()
         {
-            Servers servers = Servers.GetInstance();
+            // Жадная инициализация
+            Servers eagerServers = Servers.GetEagerInstance();
+            // Ленивая инициализация
+            Servers lazyServers = Servers.GetLazyInstance();
 
             Console.WriteLine("Добавление серверов:");
-            Console.WriteLine("Добавляем 'http://example.com': " + servers.AddServer("http://example.com"));
-            Console.WriteLine("Добавляем 'https://example.org': " + servers.AddServer("https://example.org"));
-            Console.WriteLine("Добавляем 'ftp://notallowed.com': " + servers.AddServer("ftp://notallowed.com")); // Не добавится из-за префикса
-            Console.WriteLine("Добавляем 'http://example.com': " + servers.AddServer("http://example.com")); // Дубликат - не добавится
+            Console.WriteLine("Добавляем 'http://example.com': " + lazyServers.AddServer("http://example.com"));
+            Console.WriteLine("Добавляем 'https://example.org': " + lazyServers.AddServer("https://example.org"));
+            Console.WriteLine("Добавляем 'ftp://notallowed.com': " + lazyServers.AddServer("ftp://notallowed.com")); // Не добавится из-за префикса
+            Console.WriteLine("Добавляем 'http://example.com': " + lazyServers.AddServer("http://example.com")); // Дубликат - не добавится
 
             Console.WriteLine("\nHTTP сервера:");
-            var httpServers = servers.GetHttpServers();
+            var httpServers = lazyServers.GetHttpServers();
             foreach (var server in httpServers)
             {
                 Console.WriteLine(server);
             }
 
             Console.WriteLine("\nHTTPS сервера:");
-            var httpsServers = servers.GetHttpsServers();
+            var httpsServers = lazyServers.GetHttpsServers();
             foreach (var server in httpsServers)
             {
                 Console.WriteLine(server);
